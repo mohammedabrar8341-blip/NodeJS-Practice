d6bb367a9c388620b1ee6d8815f918cc680bbed5
Mohd Abrar <your-email@example.com>
2026-07-28 16:57:30 +0530

    todobackend

--- Todoserverbackend/index.js
+++ Todoserverbackend/index.js
@@ -2,7 +2,21 @@ const express = require("express");
 
 const app = express();
 
-let todoArr = ["go to college"];
+let todoArr = ["go to university"];
+
+function updateTodo(oldTodo, newTodo) {
+  let index;
+  let filteredArray = todoArr.filter((todo, index) => {
+    if (todo === oldTodo) {
+      index = index;
+      return false;
+    } else {
+      return true;
+    }
+  });
+  filteredArray.splice(index, 0, newTodo);
+  todoArr = filteredArray;
+}
 
 // extract json from body and parse in original js object
 app.use(express.json());
@@ -14,15 +28,41 @@ app.get("/todo", (req, resp) => {
 });
 
 app.post("/todo", (req, resp) => {
-  const newTodo = req.body.todo;
+  const { data } = req.body;
+  // console.log(req.body);
 
-  todoArr.push(newTodo);
+  todoArr.push(data);
+  console.log(todoArr);
 
   resp.json({
     message: "Todo recieved and added successfully",
   });
 });
 
+app.put("/todo", (req, resp) => {
+  const { oldTodo, newTodo } = req.body;
+
+  updateTodo(oldTodo, newTodo);
+  console.log(todoArr);
+
+  resp.json({
+    message: "todo update successfully",
+  });
+});
+
+app.delete("/todo", (req, res) => {
+  const { todo } = req.body;
+  todoArr = todoArr.filter((elem) => {
+    if (elem == todo) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+  res.json({
+    message: "todo deleted successfully",
+  });
+});
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log("Server is running at port", PORT);
