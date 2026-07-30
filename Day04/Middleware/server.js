1d21a5f9a46933ddeea259724807a2dc9bf6e991
Mohd Abrar <your-email@example.com>
2026-07-30 22:28:20 +0530

    middlewares complete

--- Day04/Middleware/server.js
+++ Day04/Middleware/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const cros = require("cors");
 const restaurantArr = require("../utlis/mock_Data");
+
 const app = express();
+
+let counter = 0;
+// rate limiter counter reset
+setInterval(() => {
+  counter = 0;
+}, 5000);
 
 const addTimeStamps = (req, res, next) => {
   const now = new Date();
@@ -9,10 +17,43 @@ const addTimeStamps = (req, res, next) => {
   next();
 };
 
-app.get("/menu/:id", addTimeStamps, (req, res) => {
-  console.log("send Menu function called");
+const requestCounter = (req, res, next) => {
+  counter++;
+  console.log("current request count ", counter);
+  next();
+};
+const rateLimiter = (req, res, next) => {
+  if (counter < 6) {
+    next();
+  } else {
+    res.status(429).json({
+      msg: "limit exceed",
+    });
+  }
+};
 
-  console.log(req.newName);
+const requestLoggerMiddleware = (req, res, next) => {
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+
+  console.log(".........Request...........");
+  console.log("Method is: ", req.method);
+  console.log("Route is: ", req.url);
+  console.log("Time is: ", time);
+
+  next();
+};
+
+app.use(express.json());
+app.use(cros());
+app.use(addTimeStamps);
+app.use(requestLoggerMiddleware);
+app.use(requestCounter);
+app.use(rateLimiter);
+app.get("/menu/:id", addTimeStamps, (req, res) => {
+  // console.log("send Menu function called");
+
+  // console.log(req.newName);
   const { id } = req.params;
 
   const targetMenu = restaurantArr.filter((res) => {
@@ -25,7 +66,7 @@ app.get("/menu/:id", addTimeStamps, (req, res) => {
   });
 });
 
-app.get("/menu", addTimeStamps, (req, res) => {
+app.get("/menu", (req, res) => {
   // console.log("Restaurant menu ");
   res.json({
     messageRecievedTime: req.timeStamps,
